8c94922dd928ebc11ffc58ebccb90d2852488f2a
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-02-27 15:34:18 -0300

    jabilitando loading

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,26 @@
 // vai controlar qual rota vai ser enviada
 import React, { useContext } from "react";
+import { ActivityIndicator } from "react-native";
 import { AuthContext } from "../contexts/auth";
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
+
+import { LoadingView } from "./styles.js"
 
 export default function Routes(){
 
     const { signed, loading } = useContext(AuthContext);
 
     if(loading) {
-        
+        return(
+            <LoadingView>
+                <ActivityIndicator 
+                    size={ 60 }
+                    color="#333"
+                />
+            </LoadingView>
+        )
     }
     
     return(
