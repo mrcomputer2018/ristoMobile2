8d562779afe5bb8c9f57ce1d7a3803a164988234
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-03-11 07:03:34 -0300

    acertos de layout

--- src/pages/Reserve/index.js
+++ src/pages/Reserve/index.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 import 
 { Text, View, StyleSheet, TextInput, TouchableOpacity } 
 from "react-native";
 
 import Header from "../../components/Header";
+
+const ReserveSchema = Yup.object().shape({
+
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+ 
+    lastName: Yup.string()
+       .min(2, 'Too Short!')
+       .max(50, 'Too Long!')
+       .required('Required'),
+ 
+    email: Yup.string().email('Invalid email').required('Required'),
+ 
+  });
 
 export default function Reserve(){
 
@@ -14,83 +33,100 @@ export default function Reserve(){
     const [ hour, setHour] = useState('');
 
     return(
-        <View style={ styles.background }>
-            <Header/>
+        <Formik
+            initialValues={{
+                nameReserve:'',
+                emailReserve: '',
+                telephone: '',
+                date: '',
+                hour: '',
+            }}
+        >
+            {({values, errors, touched, handleSubmit }) => (
 
-            <View style={ styles.container }>
-                <Text style={ styles.text }>Reserva</Text>
+            <View style={ styles.background }>
+                <Header/>
 
-                <View style={ styles.viewInput }>
-                    <Text style={ styles.textLabel }>Nome:</Text>
-                    <TextInput
-                        style={ styles.input }
-                        placeholder="ex: joao silva"
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        value= { nameReserve }
-                        onChangeText={ (text) => setNameReserve(text) }
-                    />
+                <View style={ styles.container }>
+                    <Text style={ styles.text }>Reserva</Text>
+
+                    <View style={ styles.viewInput }>
+                        <Text style={ styles.textLabel }>Nome:</Text>
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="ex: joao silva"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            value= { nameReserve }
+                            onChangeText={ (text) => setNameReserve(text) }
+                        />
+                    </View>
+
+                    <View style={ styles.viewInput }>
+                        <Text style={ styles.textLabel }>E-mail:</Text>
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="ex: contato@contato.com"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            value= { emailReserve }
+                            onChangeText={ (text) => setEmailReserve(text) }
+                        />
+                    </View>
+
+                    {/* telefone */}
+                    <View style={ styles.viewInput }>
+                        <Text style={ styles.textLabel }>telefone de contato:</Text>
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="ex: (21)99999-9999"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            value= { telephone }
+                            onChangeText={ (text) => setTelephone(text) }
+                        />
+                    </View>
+
+                    {/* dia */}
+                    <View style={ styles.viewInput }>
+                        <Text style={ styles.textLabel }>Data:</Text>
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="ex: 03/03/2023"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            value= { date }
+                            onChangeText={ (text) => setDate(text) }
+                        />
+                    </View>
+
+                    {/* horario */}
+                    <View style={ styles.viewInput }>
+                        <Text style={ styles.textLabel }>Horario:</Text>
+                        <TextInput
+                            style={ styles.input }
+                            placeholder="ex: 13:00"
+                            autoCorrect={ false }
+                            autoCapitalize="none"
+                            value= { hour }
+                            onChangeText={ (text) => setHour(text) }
+                        />
+                    </View>
                 </View>
-
-                <View style={ styles.viewInput }>
-                    <Text style={ styles.textLabel }>E-mail:</Text>
-                    <TextInput
-                        style={ styles.input }
-                        placeholder="ex: contato@contato.com"
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        value= { emailReserve }
-                        onChangeText={ (text) => setEmailReserve(text) }
-                    />
-                </View>
-
-                {/* telefone */}
-                <View style={ styles.viewInput }>
-                    <Text style={ styles.textLabel }>telefone de contato:</Text>
-                    <TextInput
-                        style={ styles.input }
-                        placeholder="ex: (21)99999-9999"
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        value= { telephone }
-                        onChangeText={ (text) => setTelephone(text) }
-                    />
-                </View>
-
-                {/* dia */}
-                <View style={ styles.viewInput }>
-                    <Text style={ styles.textLabel }>Data:</Text>
-                    <TextInput
-                        style={ styles.input }
-                        placeholder="ex: 03/03/2023"
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        value= { date }
-                        onChangeText={ (text) => setDate(text) }
-                    />
-                </View>
-
-                 {/* horario */}
-                 <View style={ styles.viewInput }>
-                    <Text style={ styles.textLabel }>Horario:</Text>
-                    <TextInput
-                        style={ styles.input }
-                        placeholder="ex: 13:00"
-                        autoCorrect={ false }
-                        autoCapitalize="none"
-                        value= { hour }
-                        onChangeText={ (text) => setHour(text) }
-                    />
-                </View>
-
-               {/*  botao */}
+                
+                {/*  botao */}
                 <View style={ styles.btnView }>
-                    <TouchableOpacity style={ styles.btn }>
+                    <TouchableOpacity 
+                        style={ styles.btn }
+                        onPress={() => {}}
+                        >
                         <Text style={ styles.textBtn }>Reservar</Text>
                     </TouchableOpacity>
                 </View>
             </View>
-        </View>
+
+            )}
+        </Formik>
     );
 }
 
@@ -100,12 +136,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#eee',
     },
     container: {
-        flex: 1,
         backgroundColor: '#fafafa',
         borderRadius: 16,
-        marginRight: 30,
-        marginLeft: 30,
-        marginBottom: 65,
+        marginRight:20,
+        marginLeft:20,
+        padding: 5,
         borderWidth: 1,
         borderColor: '#ddd',
     },
@@ -118,12 +153,12 @@ const styles = StyleSheet.create({
     viewInput: {
         marginLeft: 15,
         marginRight: 15,
-        marginBottom: 15,
+        marginBottom: 10,
     },
     textLabel: {
         fontSize: 18,
         fontWeight: 'bold',
-        marginBottom: 10,
+        marginBottom: 5,
     },
     input: {
         borderWidth: 1,
